65e8438aa5720f5bc20e01d81fbf7d462fc9605f
NHNEnt <titan515@naver.com>
2018-04-12 13:56:39 +0900

    1

---
+++ Assets/Script/ImageManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageManager : MonoBehaviour {
+
+	public UnityEngine.UI.Image bg;
+	// Use this for initialization
+	void Start () {
+		System.IO.DirectoryInfo di = System.IO.DirectoryInfo.GetFiles();
+		foreach(var file in di.GetFiles())
+			Debug.Log (file.Name);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
